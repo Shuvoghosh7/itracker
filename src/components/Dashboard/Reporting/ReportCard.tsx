36a6ac9dd1b8069b4525fe83509b31398f361a7d
Shuvoghosh7 <shuvoghosh1610@gmail.com>
2022-09-04 13:26:53 +0600

    pull new code

--- src/components/Dashboard/Reporting/ReportCard.tsx
+++ src/components/Dashboard/Reporting/ReportCard.tsx
@@ -1,36 +1,15 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { MdPersonAddAlt1 } from 'react-icons/md';
 import { AiFillRead } from 'react-icons/ai';
 import { BsFillCalendarEventFill } from 'react-icons/bs';
 import { Link } from 'react-router-dom';
-import { useQuery } from '@tanstack/react-query';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import auth from '../../../firebase.init';
 const ReportCard = () => {
-    const [user, loading] = useAuthState(auth)
-    const [projects, setProjects] = useState([])
-    const [users, setUsers] = useState([])
-    useEffect(() => {
-        if (user) {
-            fetch(`https://dry-eyrie-76820.herokuapp.com/totalProject?email=${user.email}`)
-                .then(res => res.json())
-                .then(data => {
-                    setProjects(data)
-                });
-        }
-    }, [user])
-
-    useEffect(() => {
-        fetch("https://dry-eyrie-76820.herokuapp.com/getUser")
-            .then(res => res.json())
-            .then(data => setUsers(data))
-    }, [])
     return (
         <div className='grid lg:grid-cols-4 gap-6 md:grid-cols-2'>
             <div className='hover:border-2 hover:rounded-lg hover:border-slate-500 shadow-md'>
                 <div className='text-center py-8'>
                     <p className='mb-5 text-2xl font-bold mx-2'>Completed Tasks</p>
-                    <p className='mb-5 font-medium'><Link to='/dashboard/completedTasks'>7</Link></p>
+                    <p className='mb-5 font-medium'><Link to='/dashboard/completedTasks'>8</Link></p>
                 </div>
             </div>
             <div className='hover:border-2 hover:rounded-lg hover:border-slate-500 shadow-md'>
@@ -39,26 +18,17 @@ const ReportCard = () => {
                     <p className='mb-5 font-medium'><Link to='/dashboard/incompleteTasks'>7</Link></p>
                 </div>
             </div>
-
             <div className='hover:border-2 hover:rounded-lg hover:border-slate-500 shadow-md'>
-                <Link to='/dashboard/totalTasks'>
-                    <div className='text-center py-8'>
-                        <p className='mb-5 text-2xl font-bold'>Total Tasks</p>
-                        <p className='mb-5 font-medium'>{projects.length}</p>
-                    </div>
-
-
-                </Link>
+                <div className='text-center py-8'>
+                    <p className='mb-5 text-2xl font-bold'>Overdue Task</p>
+                    <p className='mb-5 font-medium'><Link to=''>4</Link></p>
+                </div>
             </div>
             <div className='hover:border-2 hover:rounded-lg hover:border-slate-500 shadow-md'>
-                <Link to='/dashboard/totalUser'>
-                    <div className='text-center py-8'>
-                        <p className='mb-5 text-2xl font-bold'>Total User</p>
-                        <p className='mb-5 font-medium'>{users.length}</p>
-                    </div>
-
-
-                </Link>
+                <div className='text-center py-8'>
+                    <p className='mb-5 text-2xl font-bold'>Total Tasks</p>
+                    <p className='mb-5 font-medium'><Link to=''>14</Link></p>
+                </div>
             </div>
         </div>
     );
